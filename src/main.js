c1737bb9bf00bea0569f082c359d3b5d72ec4333
berrynul <blueorangerino@gmail.com>
2021-07-23 19:05:23 -0700

    final comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+//Author: Nathanael Fonken 
+//Title: Burger Patrol 
+//7/23/2021
+//It took 2 days or 12~ hours to complete the project
+//Attempted points:
+//  Finishing the Tutorial: an automatic 20 points.
+//  Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+//  Implement mouse control for player movement and mouse click to fire (20)
+//  Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+//  Create and implement a new weapon (w/ new behavior and graphics) (20)
+
+//Note: I created an animation spritesheet for the spaceships dying, you can still see it even if it's obscured a bit by the particles
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
@@ -39,7 +45,7 @@ class Menu extends Phaser.Scene {
       }
         
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'BURGER PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use mouse to aim', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, "Left click to fire rocket", menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
